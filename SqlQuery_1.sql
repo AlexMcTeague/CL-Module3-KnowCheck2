e8a2b2ba7733585fc3175f2a4b19b62a603d8b8f
Flarkinater <flarkinater@gmail.com>
2023-10-28 23:29:15 -0400

    Added new SQL File

---
+++ SqlQuery_1.sql
@@ -0,0 +1,9 @@
+﻿SELECT Customers.CustomerName
+FROM Customers INNER JOIN Orders
+ON Customers.CustomerID = Orders.CustomerID
+WHERE OrderID = 10310;
+
+SELECT Suppliers.Address
+FROM Suppliers INNER JOIN Products
+ON Suppliers.SupplierID = Products.SupplierID
+WHERE ProductID = 40;
